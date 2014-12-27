6d4a50ab649f6250413041e1d91066785b0a1060
RossCodes <rosson.creighton@gmail.com>
2014-12-27 17:59:39 -0500

    Commented organized code

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!-- php includes used to simplify and organize code during development -->
+<html lang="en">
+    <head>
+        <?php include('./assets/includes/head.html'); ?>
+    </head>
+    <body>
+        
+        <!-- Fixed-top responsive navbar -->
+        
+        <?php include('./assets/includes/navbar.html'); ?>
+        
+        <!-- Header -->
+        
+        <?php include('./assets/includes/header.html'); ?>
+        
+        <!-- Main Content -->
+        
+        <?php include('./assets/includes/content.html'); ?>
+        
+        <!-- Scripts -->
+        
+        <?php include('./assets/includes/scripts.html'); ?>
+        
+    </body>
+</html>
